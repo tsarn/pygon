377e87d6da8d065672c678cc8f265571836f0311
Tsarev Nikita <nikts53@gmail.com>
2019-07-17 13:39:32 +0300

    Swapped my first and last name in copyrights

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pyyaml', 'loguru', 'click', 'setuptools', 'tabulate'
+    'pyyaml', 'loguru', 'click', 'setuptools', 'tabulate', 'requests'
 ]
 
 # What packages are optional?
